00f382b5b7669c40e74ff465982bd1ff9b42f67f
zz070 <2389655715@qq.com>
2020-04-09 20:59:11 +0800

    插入排序

--- Test/src/Sort/Insert.java
+++ Test/src/Sort/Insert.java
@@ -6,13 +6,14 @@ public class Insert {
     //升序排列
     public static void insertSort(int[] array){
         //通过bound来划分是否已排序
-        //其中[0,boung)表示已划分区域，[bound,array.length)表示未划分区域
-        for(int bound = 1;bound < array.length;bound++){
+        //其中[0,boung)表示已划分区域
+        // [bound,array.length)表示未划分区域
+        for(int bound = 1;bound < array.length;bound ++){
             int val = array[bound];
             int cur = bound -1;
             for(;cur>=0;cur--){
-                if(array[cur] >val){
-                   array[cur+1] =array[cur];
+                if(array[cur] > val){                //这个条件如果写成 >= , 插入排序就不是稳定排序了
+                    array[cur+1] = array[cur];
                 }else{
                     break;
                 }
@@ -21,7 +22,7 @@ public class Insert {
         }
     }
     public static void main(String[] args) {
-        int[] arr = {9, 5, 2, 7, 3, 6, 8};
+        int[] arr = {8, 5, 9, 7, 3, 6, 1};
          insertSort(arr);
         System.out.println(Arrays.toString(arr));
     }
