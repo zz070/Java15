27e2e80f30cfcba91ca88c719a38a5d9ae82f788
zz070 <2389655715@qq.com>
2020-04-14 22:13:37 +0800

    复制带有随机指针的链表

--- Test/src/MyMap/MapExample2.java
+++ Test/src/MyMap/MapExample2.java
@@ -20,9 +20,14 @@ public class MapExample2 {
     //复制带有随机指针的链表
 
     /*
-    1、遍历原链表，将原链表插入到map中，key表示就链表的结点，value表示拷贝出来的新结点
-    2、再次遍历链表，修改新链表中的next和random
-    3、最后，新链表的头结点即可
+    题目描述:
+            给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+            要求返回这个链表的 深拷贝。
+
+    思路：
+            1、遍历原链表，将原链表插入到map中，key表示就链表的结点，value表示拷贝出来的新结点
+            2、再次遍历链表，修改新链表中的next和random
+            3、最后，新链表的头结点即可
      */
     public Node copyRandomList(Node head){
         Map<Node,Node> map = new HashMap<>();
