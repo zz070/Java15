4d98e1589267d7273f1e8a02013ce08a25010848
zz070 <2389655715@qq.com>
2020-04-29 11:50:31 +0800

    选择排序

--- Test/src/Sort/Select.java
+++ Test/src/Sort/Select.java
@@ -8,7 +8,7 @@ public class Select {
     //升序排列
     public static void selectSort(int[] array){
         //以bound位置元素作为擂主，依次取未排序区域元素与之比较，打擂成功即交换位置
-        for(int bound = 0;bound<array.length;bound++){
+        for(int bound = 0;bound<array.length-1;bound++){
             for(int cur = bound + 1;cur<array.length;cur++){
                 if(array[cur] < array[bound]){
                     int temp = array[cur];
@@ -20,7 +20,7 @@ public class Select {
     }
 
     public static void main(String[] args) {
-        int[] arr = {9, 5, 0, 7, 8,3, 6, 1};
+        int[] arr = {9, 5,4, 0, 7, 2,8,3, 6, 1};
         selectSort(arr);
         System.out.println(Arrays.toString(arr));
     }
