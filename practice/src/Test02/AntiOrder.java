dca0a99fe283b59aace6ae75593be43e06dfbb7c
zz070 <2389655715@qq.com>
2020-06-04 19:55:10 +0800

    数组的逆序对

---
+++ practice/src/Test02/AntiOrder.java
@@ -0,0 +1,17 @@
+package Test02;
+
+import java.util.*;
+
+public class AntiOrder {
+    public int count(int[] A, int n) {
+        int count = 0;
+       for(int i  = 0;i < A.length;i++){
+           for(int j = i+1;j<A.length;j++){
+               if(A[i]>A[j]){
+                   count++;
+               }
+           }
+       }
+       return count;
+    }
+}
