551e0522430fa69f069692709e1b6c7e6d5ab53c
zz070 <2389655715@qq.com>
2020-06-07 20:08:35 +0800

    AB最小公倍数

--- practice/src/Test07/B.java
+++ practice/src/Test07/B.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            int n = A>B?A:B;
-            for(int i = n;i<=A*B;++){
-                if(i%A==0&&i%B==0){
-                    System.out.println(i);
-                    break;
-                }
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int n = A > B ? A : B;
+        for (int i = n; i <= A * B; i++) {
+            if (i % A == 0 && i % B == 0) {
+                System.out.println(i);
+                break;
             }
         }
     }
