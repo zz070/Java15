e20adfcca60eac869182f8427c209d015fdc6612
zz070 <2389655715@qq.com>
2020-06-25 19:00:00 +0800

    密码问题

--- practice/src/Test21/problem.java
+++ practice/src/Test21/problem.java
@@ -6,13 +6,14 @@ public class problem {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()){
             char[] c = sc.nextLine().toCharArray();
-            char[] cc = new char[c.length];
             for(int i = 0;i< c.length;i++){
-                if(cc[i]>'A'&&cc[i]<'Z'){
-                    cc[i] = (char) (c[i]>'E'?(c[i]-5):(c[i]+21));
+                char s = c[i];
+                if(c[i]>='A'&&c[i]<='Z'){
+                    s = (char) (s>'E'?(s-5):(s+21));
                 }
+                c[i] = s;
             }
-            System.out.println(cc.toString());
+            System.out.println(new String(c));
         }
     }
 }
