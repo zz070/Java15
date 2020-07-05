1403ab81728d38851015745c91a0bf83b0f11568
zz070 <2389655715@qq.com>
2020-07-05 19:44:02 +0800

    逆序字符串

---
+++ practice/src/Test31/Test.java
@@ -0,0 +1,19 @@
+package Test31;
+
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String s = sc.nextLine();
+            String[] ss = s.split(" ");
+            for(int i = ss.length-1;i >=0 ;i--){
+                if(i!=0){
+                    System.out.print(ss[i]+" ");
+                }else{
+                    System.out.print(ss[i]);
+                }
+            }
+        }
+    }
+}
