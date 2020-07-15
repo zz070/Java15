75f4959bbfc4050a2cd53a0ca72613df800f0752
zz070 <2389655715@qq.com>
2020-07-15 22:21:31 +0800

    删除公共字符串

---
+++ practice/src/Test39/Character.java
@@ -0,0 +1,21 @@
+package Test39;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Character {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+            String d = sc.nextLine();
+            ArrayList<Character> list = new ArrayList<>();
+            for(int i = 0;i<s.length();i++){
+                if(!(d.contains(s.charAt(i)+""))){
+                    list.add(s.charAt(i));
+                }
+            }
+            for(int i = 0;i<list.size();i++){
+                System.out.println(list.get(i)+"");
+            }
+        }
+}
