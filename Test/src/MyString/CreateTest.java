75af9193c30e135569b0563d618f1c6ef22caf6e
zz070 <2389655715@qq.com>
2020-07-16 21:24:54 +0800

    创建字符串

---
+++ Test/src/MyString/CreateTest.java
@@ -0,0 +1,13 @@
+package MyString;
+
+public class CreateTest {
+    // 方式一
+    String s = "Hello World";
+
+    // 方式二
+    String s2 = new String("Hello World");
+
+    // 方式三
+    char[] array = {'a', 'b', 'c'};
+    String s3 = new String(array);
+}
