f2fd5e2cae0df61d5f48dfe9a43398f322785b05
zz070 <2389655715@qq.com>
2020-07-17 16:42:56 +0800

    字符串截取

---
+++ Test/src/MyString/SubTest.java
@@ -0,0 +1,9 @@
+package MyString;
+
+public class SubTest {
+    public static void main(String[] args) {
+        String s = "hello world ! ByeBye";
+        System.out.println(s.substring(6));
+        System.out.println(s.substring(0,12));
+    }
+}
