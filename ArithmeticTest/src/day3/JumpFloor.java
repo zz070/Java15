73f8b4458a9777394b465c5410b6d98f275ef0d5
zz070 <2389655715@qq.com>
2020-07-22 17:36:30 +0800

    跳台阶问题

--- ArithmeticTest/src/day3/JumpFloor.java
+++ ArithmeticTest/src/day3/JumpFloor.java
@@ -1,6 +1,16 @@
 package day3;
 
 public class JumpFloor {
+    //简单的动态规划
+    public static int jump1(int n){
+        int[] step = new int[n+1];
+        step[0] = 1;
+        step[1] = 1;
+        for(int i = 2;i<=n;i++){
+            step[i] = step[i-1]+step[i-2];
+        }
+        return step[n];
+    }
     //台阶问题，同样可以转化为斐波那契数列问题
 
     private static int jump(int n) {
@@ -23,5 +33,7 @@ public class JumpFloor {
     }
     public static void main(String[] args) {
         System.out.println(jump(10));
+        System.out.println(jump1(10));
+
     }
 }
