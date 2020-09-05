4b862bc22a70528dd06cfb8b79a0bba1791e5871
zz070 <2389655715@qq.com>
2020-09-05 22:26:40 +0800

    无变量交换数字

---
+++ practice/src/Test12/Exchange.java
@@ -0,0 +1,13 @@
+package Test12;
+
+import java.util.*;
+
+public class Exchange {
+    public int[] exchangeAB(int[] AB) {
+        // write code here
+        AB[0] = AB[0]+AB[1];
+        AB[1] = AB[0] - AB[1];
+        AB[0] = AB[0] - AB[1];
+        return AB;
+    }
+}
